a7ed1b18425ec51d264e8362fb9791c51f64404c
Peter Clark <peterlc2@illinois.edu>
2020-06-30 02:05:52 -0400

    Updating youtube to allow for proper searching

--- commands/yt.js
+++ commands/yt.js
@@ -19,18 +19,70 @@ module.exports = {
 			let url;
 			if (!query.startsWith(youtubePrefix)) {
 				const res = await youtube.search.list({
-					part: 'id',
+					part: 'snippet, id',
 					q: query
 				});
 
-				const id = res.data.items[0].id.videoId;
+				const identifierEmoji = ['🔴','🟠','🟡','🟢','🔵'];
+				let response = '';
+				for (let i = 0; i < res.data.items.length; i++) {
+					const videoData = res.data.items[i].snippet;
+					response += identifierEmoji[i] + ' ' + videoData.title + ' - ' + videoData.channelTitle + '\n';
+				}
+				const videoList = await message.channel.send(response);
+				try {
+					for (let i = 0; i < identifierEmoji.length; i++) {
+						await videoList.react(identifierEmoji[i]);
+					}
+				} catch (error) {
+					console.error('Failed to react with an emoji');
+				}
+
+				const filter = (reaction, user) => {
+					return identifierEmoji.includes(reaction.emoji.name) && user.id != videoList.author.id
+					;
+				}
+				const collector = videoList.createReactionCollector(filter, { max: 1, time: 15000 });
+
+				const selectionPromise = new Promise(resolve => {
+					collector.on('collect', (reaction, user) => {
+						const index = identifierEmoji.findIndex(x => x == reaction.emoji.name);
+						if(index == -1) {
+							console.error('reaction not in list?');
+							index = 0;
+						}
+						resolve(index);
+					});
+				});
+
+				const timeoutPromise = new Promise(resolve => {
+					collector.on('end', collected => {
+						if(collected.size == 0) console.log('yt: timeout triggered on song select');
+						resolve(collected.size);
+					});
+				});
+
+				const resultPromise = Promise.race([selectionPromise, timeoutPromise]);
+
+				const id = res.data.items[await resultPromise].id.videoId;
+				videoList.delete();
 				url = `${youtubePrefix}${id}`;
 			} else {
 				url = query;
 			}
-			const stream = ytdl(url, {
-				filter: 'audioonly'
-			});
+
+			let stream;
+			try {
+				stream = ytdl(url, {
+					filter: 'audioonly'
+				});
+			} catch(error) {
+				console.error('Failed to download youtube media');
+			}
+
+			message.channel.send('Now playing ' + url);
+
+			const dispatcher = connection.play(stream);
 
 			dispatcher.on('finish', () => {
 				connection.disconnect();
